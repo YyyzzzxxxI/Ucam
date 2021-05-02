24f111794d90f448fc864e2b816451efee4e4fc5
YzxI <testwarfase@mail.ru>
2021-05-02 23:49:19 +0300

    Dark theme

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,26 +1,36 @@
-import React, {useEffect} from 'react';
 import * as eva from '@eva-design/eva';
-import {EvaIconsPack} from "@ui-kitten/eva-icons";
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from "@ui-kitten/eva-icons";
 
 import {StatusBar} from 'expo-status-bar';
+import {observer} from "mobx-react"
+import React from 'react';
+import {StyleSheet, View} from "react-native";
 import {Navbar} from "./src/components/Navbar/Navbar";
-import videosStore from "./src/store/videos.store";
-import profileStore from "./src/store/profile.store";
-import params from "./src/views/params";
+import {profileStore} from "./src/store/profile.store"
+
+const AppProvider = observer(() => {
+    return (
+        <ApplicationProvider  {...eva} theme={profileStore.darkMode ? eva.dark : eva.light}>
+            <Navbar/>
+        </ApplicationProvider>
+    )
+})
 
 export default function App() {
-
-    useEffect(() => {
-    }, [])
-
     return (
-        <>
+        <View style={styles.container}>
             <StatusBar style={"auto"} translucent={true}/>
             <IconRegistry icons={EvaIconsPack}/>
-            <ApplicationProvider  {...eva} theme={eva.light}>
-                <Navbar/>
-            </ApplicationProvider>
-        </>
+            <AppProvider/>
+        </View>
     )
 }
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1
+        }
+    }
+)

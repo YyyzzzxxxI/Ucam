9509cfd60b7be10d1ff1856d406de065a4d4b332
YzxI <testwarfase@mail.ru>
2021-05-02 23:47:30 +0300

    Added dark theme, first login logic

--- frontend/src/store/profile.store.ts
+++ frontend/src/store/profile.store.ts
@@ -1,22 +1,28 @@
-import {makeAutoObservable, runInAction} from "mobx";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as FileSystem from 'expo-file-system';
+import {makeAutoObservable, runInAction} from "mobx";
+import {profileAsyncStorageItems, videosAsyncStorageItems, videosFolder} from "../views/params";
 
 
 class ProfileStore {
     username = ''
     isLogged = false
+    isFirstLogin = true
+    darkMode = false
 
     constructor() {
         makeAutoObservable(this)
     }
 
     async init() {
-        this.username = await AsyncStorage.getItem("username") || ""
+        this.username = await AsyncStorage.getItem(profileAsyncStorageItems.username) || ""
+        this.darkMode = !!Number(await AsyncStorage.getItem(profileAsyncStorageItems.darkMode))
+        if (this.username != "") this.isFirstLogin = false
         console.log("init " + this.username)
     }
 
     async register(username: string) {
-        await AsyncStorage.setItem("username", username)
+        await AsyncStorage.setItem(profileAsyncStorageItems.username, username)
         runInAction(() => {
             this.username = username
             this.isLogged = true
@@ -24,24 +30,39 @@ class ProfileStore {
     }
 
     async login(username: string) {
-        await AsyncStorage.setItem("username", username)
+        await AsyncStorage.setItem(profileAsyncStorageItems.username, username)
         runInAction(() => {
             this.username = username
             this.isLogged = true
         })
     }
 
+    async setDarkMode(enabled: boolean) {
+        runInAction(() => {
+            this.darkMode = enabled
+        })
+        await AsyncStorage.setItem("darkMode", (+enabled).toString())
+    }
+
     async erase() {
-        await AsyncStorage.clear()
+        await AsyncStorage.removeItem(profileAsyncStorageItems.username)
+        await AsyncStorage.removeItem(videosAsyncStorageItems.videos)
+        await AsyncStorage.removeItem(videosAsyncStorageItems.videoKey)
+        if (videosFolder != null) {
+            await FileSystem.deleteAsync(videosFolder)
+        }
         runInAction(() => {
             this.username = ""
             this.isLogged = false
+            this.isFirstLogin = true
+            this.darkMode = false
         })
+        console.log("Profile and videos erased")
     }
 
 }
 
-let p = new ProfileStore()
-p.init()
+let profileStore = new ProfileStore()
+profileStore.init()
 
-export default p
+export {profileStore}

801d438d3a87426a4cf4e94a7ff983d2f851ae9e
YzxI <testwarfase@mail.ru>
2021-05-02 23:46:33 +0300

    Added more config options

--- frontend/src/views/params.ts
+++ frontend/src/views/params.ts
@@ -1,5 +1,7 @@
-// views
-export default {
+import * as FileSystem from "expo-file-system";
+
+
+const views = {
     HOME: 'Home',
     CAMERA: 'Camera',
     PROFILE: 'Profile',
@@ -7,3 +9,23 @@ export default {
     LOGIN: "Login",
     REGISTER: "Register"
 }
+
+const server = {
+    SERVER_URI: "http://a3420502c5bb.ngrok.io/"
+}
+
+const profileAsyncStorageItems = {
+    username: "username",
+    darkMode: "darkMode"
+}
+
+const videosAsyncStorageItems = {
+    videos: "videos",
+    videoKey: "videoKey"
+}
+
+const videosFolder = FileSystem.documentDirectory + "videos/"
+
+
+export {views, server, profileAsyncStorageItems, videosAsyncStorageItems, videosFolder}
+
